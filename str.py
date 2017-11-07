0d3678e2224a2d1709b73d1121db4471c3d80a88
telishubham <32251001+telishubham@users.noreply.github.com>
2017-11-07 15:07:38 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,14 @@
+s1="XYZ"
+s2="@"
+s3="abc.com"
+str=s1+s2+s3
+print(str)
+print(str.upper())
+print(str.lower())
+print(str.strip())
+print(str.count('c'),"is the count of c in string")
+print(str.index("c"),"is the index of c")
+print("do string contain only alphabets:",str.isalpha())
+print(str.replace("c","p"))
+print(str.swapcase(),"string case is swapped")
+print(str.title(),"title case of string")
